9230f97603248d26a556c22ef4456787ee83090d
Stiven Ramírez Arango <sramir70@eafit.edu.co>
2019-07-09 16:56:11 -0500

    Adding components

---
+++ udemy-1/pages/blog/index.vue
@@ -0,0 +1,10 @@
+<template>
+    <div class="container">
+        <ul>
+            <li>Artículo #1</li>
+            <li>Artículo #2</li>
+            <li>Artículo #3</li>
+            <li>Artículo #4</li>
+        </ul>
+    </div>
+</template>
